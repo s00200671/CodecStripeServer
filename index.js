1d5448f7fcec64d7fabc9f8978f4c12375df48db
Ronnie Conlon <S00200671@mail.itsligo.ie>
2022-03-01 14:54:36 +0000

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3000
-const stripe = require("Stripe");
+const stripe = require("stripe")(process.env["STRIPE_SK"]);
 
 app.get("/", async (req, res) => {console.log("hello"), res.json({stauts: 200})})
 app.post('/create-checkout-session', async (req, res) => {
